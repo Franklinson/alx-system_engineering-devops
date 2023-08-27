02d888dd44912d1f04b4629548ce2c6922853161
Franklinson <franklinhassey@gmail.com>
2023-08-27 15:06:13 -0700

    config using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,11 @@
 class ssh {
   $private_key_file = '~/.ssh/school'
 
-  # Configure the SSH client to use the private key
   ssh_config {
     'default':
       identity_file => $private_key_file,
   }
 
-  # Refuse to authenticate using a password
   ssh_config {
     '*':
       password_authentication => 'no',
