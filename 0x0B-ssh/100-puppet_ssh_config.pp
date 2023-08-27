c922aea501dff0a86f98817e88c9e9de829cd375
Franklinson <franklinhassey@gmail.com>
2023-08-27 15:19:30 -0700

    puppet with config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 # puppet to create a config file
 
-class ssh {
-  $private_key_file = '~/.ssh/school'
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
 
-  ssh_config {
-    'default':
-      identity_file => $private_key_file,
-  }
-
-  ssh_config {
-    '*':
-      password_authentication => 'no',
-  }
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
