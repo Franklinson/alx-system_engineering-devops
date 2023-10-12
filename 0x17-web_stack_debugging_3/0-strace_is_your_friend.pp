d1c7d0cb17d2f317ac32568ceeab192afaa39ae7
Franklinson <franklinhassey@gmail.com>
2023-10-12 00:29:20 +0000

    fix 500 error on Apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
 #Fix Apache is returning a 500 error.
 
-file { '/var/www/html/wp-settings.php':
-  ensure => file,
-  notify => Exec['replace_wp_settings'],
-}
-
-exec { 'replace_wp_settings':
-  command     => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path        => '/bin:/usr/bin',
-  refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
